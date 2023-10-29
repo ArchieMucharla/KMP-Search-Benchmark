663f044b434a2f2a05771ab02ac698a33d158334
Simone-Nicole Angelov <65798157+simoneangelov@users.noreply.github.com>
2023-10-29 17:21:36 -0500

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,58 +1,47 @@
 #include <catch2/catch_test_macros.hpp>
 
-#include "blackbox.h"
+#include "KMP.h"
 
 #include <sstream>
 
 /**
- * Reads in the dataset smallset and searches for a known matching pattern CACATCTA
- * The pattern exists (1) time in the csv on row 1:
- * TGATTTTAAAAAAACACTTAACACATCTAGATAGAATAGTACTCTGCCCTATTTGAGGGAACAGTCTCAAACNATGAAGTACATGATATTTAATGCCCTA
+ * Reads in the dataset and searches for the pattern "latin pop"
+ * The pattern exists multiple times in the csv under the playlist_subgenre column.
  */
-TEST_CASE("Descriptive Name 1", "[weight=5]")
+TEST_CASE("Find Multiple Occurrences of 'latin pop'", "[weight=5]")
 {
-	bool expected = true;
-    std::string infile = "../../data/smallset.csv";
-    std::string search = "CACATCTA";
-    bool out = blackbox_exists(infile, search);
+    int expected = 6;  // The pattern "latin pop" appears 6 times in your data set
+    std::string infile = "../../data/your-dataset.csv";
+    std::string search = "latin pop";
+    int out = blackbox_count(infile, search);
 
     REQUIRE(expected == out);
 }
 
 /**
- * Reads in the dataset smallset and searches for a known non-matching pattern GGGGGGG
+ * Reads in the dataset and searches for the pattern "Ariana Grande"
+ * The pattern exists one time in the csv under the track_artist column.
+ */
+TEST_CASE("Find Single Occurrence of 'Ariana Grande'", "[weight=5]")
+{
+    int expected = 1;  // The pattern "Ariana Grande" appears 1 time in your data set
+    std::string infile = "../../data/your-dataset.csv";
+    std::string search = "Ariana Grande";
+    int out = blackbox_count(infile, search);
+
+    REQUIRE(expected == out);
+}
+
+/**
+ * Reads in the dataset and searches for a pattern that does not exist "XYZZYX"
  * The pattern does not exist in the csv.
  */
-TEST_CASE("Descriptive Name 2", "[weight=5]")
+TEST_CASE("No Occurrence of Non-Existent Pattern", "[weight=5]")
 {
-	bool expected = false;
-    std::string infile = "../../data/smallset.csv";
-    std::string search = "AAAAAAAAAA";
-    bool out = blackbox_exists(infile, search);
-
-    REQUIRE(expected == out);
-}
-
-/**
- * Real tests will include things you havent implemented yet
- * For example, I know that I have not yet written blackbox_count yet
- * but I have a good idea of what the input / output of this function should be...
- * 
- * Reads in the dataset smallset and searches for a known matching pattern CCTT
- * The pattern exists (51) time in the csv. Accordingly it should pass a test to find it 51 times
- * and fail to find it 52 times.
- */
-TEST_CASE("Descriptive Name 3", "[weight=5]")
-{
-	bool expected = true;
-    std::string infile = "../../data/smallset.csv";
-    std::string search = "CCTT";
-    bool out = blackbox_count(infile, search, 51);
-
-    REQUIRE(expected == out);
-
-    expected = false;
-    out = blackbox_count(infile, search, 52);
+    int expected = 0;  // The pattern "XYZZYX" does not appear in your data set
+    std::string infile = "../../data/your-dataset.csv";
+    std::string search = "XYZZYX";
+    int out = blackbox_count(infile, search);
 
     REQUIRE(expected == out);
 }
