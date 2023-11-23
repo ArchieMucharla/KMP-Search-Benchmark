3a020fded5508c39bee86df5506ae40beb2d8b30
Simone-Nicole Angelov <65798157+simoneangelov@users.noreply.github.com>
2023-11-23 20:50:47 +0000

    Got tests to compile!

---
+++ build/test_tests-b12d07c.cmake
@@ -0,0 +1,3 @@
+add_test( [==[Preprocessing and Single Match]==] [==[/workspaces/CS 225/CS_225_EC/build/test]==] [==[Preprocessing and Single Match]==]  )
+set_tests_properties( [==[Preprocessing and Single Match]==] PROPERTIES WORKING_DIRECTORY [==[/workspaces/CS 225/CS_225_EC/build]==])
+set( test_TESTS [==[Preprocessing and Single Match]==])
